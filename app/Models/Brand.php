28e5e4950a20db11d07266935c8fd82c04d8a816
Mauro Welzel <maurowelzelc@hotmail.com>
2025-06-08 16:11:04 -0400

    Initialize Laravel project and add Brand entity with migration

---
+++ app/Models/Brand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    protected $primaryKey = 'brand_id';
+
+    protected $fillable = [
+        'brand_name',
+        'brand_image',
+        'rating',
+        'country_code',
+    ];
+}
